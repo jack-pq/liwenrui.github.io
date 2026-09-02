6e6a67ca9128798a5faaa91c8f35185edb23e47f
LiWenRui <2026672048@qq.com>
2026-09-02 14:13:18 +0800

    1

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: "/liwenrui",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
   experimental: {
     optimizePackageImports: ["lucide-react", "recharts"],
   },
